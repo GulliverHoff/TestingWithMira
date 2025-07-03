969b7855c8dbff5ae95f98dfd3546aaed0ea2ee2
GulliverHoff <jacob.hoffan@gmail.com>
2025-07-03 22:51:31 +0200

    testing unordered set

--- GoEngine/src/engine/include/GoEngine.hpp
+++ GoEngine/src/engine/include/GoEngine.hpp
@@ -1,28 +1,42 @@
 #include <unordered_set>
+#include <vector>
+#include <cstdint>
 
 namespace GoEng{
 
-struct Point;
-struct Chain;
-struct BoardState;
+    struct Point;
+    struct Chain;
+    struct BoardState;
 
-enum Color;
+    enum Color {
+        None = -1,
+        Black = 0,
+        White = 1
+    };
 
+    struct Point {
+        uint32_t row;
+        uint32_t col;
 
-enum Color {
-  Black = 0,
-  White = 1
-};
+        bool operator==(const Point& other) const {
+            return row == other.row && col == other.col;
+        }
+    };
 
-struct Point {
-  unsigned int row;
-  unsigned int col;
-};
+    using PointSet = std::unordered_set<Point>;
 
-using PointSet = std::unordered_set<Point>;
+    struct Chain {
+        
+    };
 
-struct Chain {
-
-};
+    struct BoardState {
+    public:
+        BoardState(uint32_t width, uint32_t height);
+    private:
+        std::vector<std::vector<Color>> data;
+        uint32_t width;
+        uint32_t height;
+    };
+        
 
 }

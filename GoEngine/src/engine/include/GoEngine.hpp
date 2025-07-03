2901411c622f8a26e0fbfe9d2438184da1918803
GulliverHoff <jacob.hoffan@gmail.com>
2025-07-03 22:22:55 +0200

    updated types

---
+++ GoEngine/src/engine/include/GoEngine.hpp
@@ -0,0 +1,28 @@
+#include <unordered_set>
+
+namespace GoEng{
+
+struct Point;
+struct Chain;
+struct BoardState;
+
+enum Color;
+
+
+enum Color {
+  Black = 0,
+  White = 1
+};
+
+struct Point {
+  unsigned int row;
+  unsigned int col;
+};
+
+using PointSet = std::unordered_set<Point>;
+
+struct Chain {
+
+};
+
+}
